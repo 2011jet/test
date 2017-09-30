c6c9e77aae9ce983fedb141f835a2ee1627989f0
Brandt Ross <brandt_ross@northlandchristian.org>
2017-09-30 09:57:08 -0500

    Default
    This is the default code that comes with the FTC App Master

---
+++ ftc_app-master/FtcRobotController/src/main/java/AdvancedProgramming.java
@@ -0,0 +1,13 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Brandt_Ross on 9/30/17.
+ */
+
+public class AdvancedProgramming extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
